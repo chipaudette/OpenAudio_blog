00abe0c366a6c94a4a67febca765d76be6881bd0
Chip Audette <chipaudette@yahoo.com>
2017-03-14 23:11:26 -0400

    Tympan Self-Noise: adding materials for blog post

--- 2017-03-11 Teensy and Tympan Self Noise/Data/analyzeUSBAudio_many.m
+++ 2017-03-11 Teensy and Tympan Self Noise/Data/analyzeUSBAudio_many.m
@@ -5,7 +5,7 @@ in_gain_dB = [];
 p_root1 = 'C:\Creare_Data\Cool Aid\Olympus Stuff\Data\20161022 Teensy 3.6 Self Noise (Home)\';
 p_root2 = 'C:\Creare_Data\Cool Aid\Olympus Stuff\Data\20170223 CoolAid Pre-AAS conference\';
 
-switch 2
+switch 4
     case 1
         % plot just those cases with similar full scale values, 0.56 pp vs 0.48 pp
         all_cases = [3 13 22 32 42]; summary_mode = 1;summary_types={'Teensy Audio' 'Tympan Audio'};
@@ -19,7 +19,22 @@ switch 2
             summary_xlt{I} = {['Line-In Level = ' num2str(line_in_levels(I))];
                               ['Max In = ' num2str(max_in_Vrms(I),2) ' Vrms']};
         end;
-       
+    case 4
+        % plot all Tympan Cases
+        all_cases = [11 12 13 14 31 32 33 34 41 42 43 44]; 
+        summary_mode = 3; summary_types = {'Teensy (no ADC HP)' 'Tympan (USB)' 'Tympan (SD)'};
+        teensy_line_in = [0 5 10 15];
+        teensy_gain_dB = 10*log10((3.12./[3.12 1.33 0.56 0.24]).^2);
+        tympan_gain_dB = [0 10 20 30];
+        line_in_levels = [teensy_gain_dB(:);tympan_gain_dB(:);tympan_gain_dB(:)];
+        max_in_Vrms = [ [[3.12 1.33 0.56 0.24]'/2/sqrt(2)];
+                        [cal_Tympan_V_atFS / sqrt(2) ./ sqrt(10.^(0.1*tympan_gain_dB(:)))];
+                        [cal_Tympan_V_atFS / sqrt(2) ./ sqrt(10.^(0.1*tympan_gain_dB(:)))]; ]
+        summary_xlt={};
+%         for I=1:length(line_in_levels); 
+%             summary_xlt{I} = {['Gain = ' num2str(line_in_levels(I)) ' dB'];
+%                               ['Max In = ' num2str(max_in_Vrms(I),2) ' Vrms']};
+%         end;
 end
 
 %%
@@ -156,7 +171,7 @@ for Icase = 1:length(all_cases)
             ind = 2;
             pname = 'TympanRawData\USB\';
             fname = '02-ShortedInputs_SteppedGain_0_10_20_30_40dB.wav'; t_start_sec = 14.4;in_gain_dB = [0 10 20 30];
-            sname = 'Tympan, Shorted Inputs, Saved via USB Audio';    gain_dur_sec = 3.05;
+            sname = {'Tympan, Shorted Inputs';['Saved via USB Audio, Gain ' num2str(in_gain_dB(ind)) ' dB']};    gain_dur_sec = 3.05;
             s_sname = {'Tympan Audio';'Shorted Inputs';'USB Audio'};
             t_lim_sec = t_start_sec + gain_dur_sec*[ind-1 ind]+[0.2 -0.2]; in_gain_dB = in_gain_dB(ind);
             cal_V_atFS = cal_Tympan_V_atFS / sqrt(10.^(0.1*in_gain_dB));  %from pre-AAS testing
@@ -169,7 +184,14 @@ for Icase = 1:length(all_cases)
             s_sname = {'Tympan Audio';'Shorted Inputs';'USB Audio'};
             t_lim_sec = t_start_sec + gain_dur_sec*[ind-1 ind]+[0.2 -0.2]; in_gain_dB = in_gain_dB(ind);
             cal_V_atFS = cal_Tympan_V_atFS / sqrt(10.^(0.1*in_gain_dB));  %from pre-AAS testing
-            
+         case 34
+            ind = 4;
+            pname = 'TympanRawData\USB\';
+            fname = '02-ShortedInputs_SteppedGain_0_10_20_30_40dB.wav'; t_start_sec = 14.4;in_gain_dB = [0 10 20 30];
+            sname = 'Tympan, Shorted Inputs, Saved via USB Audio';    gain_dur_sec = 3.05;
+            s_sname = {'Tympan Audio';'Shorted Inputs';'USB Audio'};
+            t_lim_sec = t_start_sec + gain_dur_sec*[ind-1 ind]+[0.2 -0.2]; in_gain_dB = in_gain_dB(ind);
+            cal_V_atFS = cal_Tympan_V_atFS / sqrt(10.^(0.1*in_gain_dB));  %from pre-AAS testing           
         case 41
             ind = 1;
             pname = 'TympanRawData\SD\';
@@ -183,7 +205,8 @@ for Icase = 1:length(all_cases)
             ind = 2;
             pname = 'TympanRawData\SD\';
             fname ='02-ShortedInputs_InputGain_0_10_20_30_40dB_USBdisconnected.RAW'; t_start_sec = 0;in_gain_dB = [0 10 20 30];
-            sname = 'Tympan, Shorted Inputs, Saved to SD, No USB';    gain_dur_sec = 3.;
+            %sname = 'Tympan, Shorted Inputs, Saved to SD, No USB';    gain_dur_sec = 3.;
+            sname = {'Tympan, Shorted Inputs';['Saved to SD, Gain ' num2str(in_gain_dB(ind)) ' dB']};  gain_dur_sec = 3.;
             s_sname = {'Tympan Audio';'Shorted Inputs';'SD Card'};
             t_lim_sec = t_start_sec + gain_dur_sec*[ind-1 ind]+[0.2 -0.2]; in_gain_dB = in_gain_dB(ind);
             cal_V_atFS = cal_Tympan_V_atFS / sqrt(10.^(0.1*in_gain_dB));  %from pre-AAS testing
@@ -196,7 +219,14 @@ for Icase = 1:length(all_cases)
             s_sname = {'Tympan Audio';'Shorted Inputs';'SD Card'};
             t_lim_sec = t_start_sec + gain_dur_sec*[ind-1 ind]+[0.2 -0.2]; in_gain_dB = in_gain_dB(ind);
             cal_V_atFS = cal_Tympan_V_atFS / sqrt(10.^(0.1*in_gain_dB));  %from pre-AAS testing
-            
+        case 44
+            ind = 4;
+            pname = 'TympanRawData\SD\';
+            fname = '02-ShortedInputs_InputGain_0_10_20_30_40dB_USBdisconnected.RAW'; t_start_sec =0;in_gain_dB = [0 10 20 30];
+            sname = 'Tympan, Shorted Inputs, Saved to SD, No USB';    gain_dur_sec = 3.;
+            s_sname = {'Tympan Audio';'Shorted Inputs';'SD Card'};
+            t_lim_sec = t_start_sec + gain_dur_sec*[ind-1 ind]+[0.2 -0.2]; in_gain_dB = in_gain_dB(ind);
+            cal_V_atFS = cal_Tympan_V_atFS / sqrt(10.^(0.1*in_gain_dB));  %from pre-AAS testing            
     end
     
     
@@ -223,7 +253,7 @@ for Icase = 1:length(all_cases)
     %cal_V_per_count = cal_Tympan_V_per_count / sqrt(10.^(0.1*in_gain_dB));  %from pre-AAS testing
     Nfft = 2*round(0.125*fs_Hz);
     if (1)
-        bp_Hz = [125 8000];bp_txt = [num2str(bp_Hz) '-' num2str(bp_Hz) ' Hz'];
+        bp_Hz = [125 8000];bp_txt = [num2str(bp_Hz(1)) '-' num2str(bp_Hz(2)) ' Hz'];
     else
         bp_Hz = 'A-weight'; bp_txt = ['A-weighted'];
     end
@@ -343,12 +373,13 @@ box on
 xlim([min(x) max(x)]+0.5*[-1 1]);
 ylabel({'Self Noise, Input Referred';['(dBVrms, ' bp_txt ')']});
 set(gca,'Xtick',unique(x),'XtickLabels',[]);
+title('Measured Self Noise, Input Referred');
+ylim([-120 -60]);
 for I=1:length(summary_xlt);
     yl=ylim;
     h = text(x(I),yl(1)-0.04*diff(yl), summary_xlt{I}, ...
         'Rotation',90,'VerticalAlignment','middle','horizontalAlignment','right');
 end
-title('Measured Self Noise, Input Referred');
 if (summary_mode == 2); legend(summary_types);end
 
 subplot(2,2,2);
@@ -371,6 +402,85 @@ for I=1:length(summary_xlt);
 end
 title('Instantaneous Dynamic Range of Input');
 if (summary_mode == 2); legend(summary_types);end
+
+%% same plot but with quantitative x-axis
+if summary_mode == 3
+    figure;setFigureTallWide;
+    subplot(2,2,1);
+    x = max_in_Vrms(:);
+    all_rms_fwav_V = [all_rms_wav(:).fwav_Vrms];
+    type=[];
+    ntypes = length(summary_types);
+    for I=1:ntypes;
+        type = [type(:); I*ones(size(x,1)/ntypes,1)];
+    end
+    for Itype=1:ntypes
+        I=find(type==Itype);
+        semilogx(x(I),20*log10(all_rms_fwav_V(I)),'o-','linewidth',2);
+        hold on;
+    end
+    hold off;
+    box on
+    ylim([-120 -60]-5);
+    ylabel({'Self Noise, Input Referred';['(dBVrms, ' bp_txt ')']});
+    xlim([0.01 1.5]);
+    xlabel('Maximum Allowed Input (Vrms) for Gain Setting');
+    set(gca,'XTick',[0.01 0.03 0.1 0.3 1.0],'XTIckLabel',{'0.01' '0.03' '0.1' '0.3' '1.0'});
+    title('Measured Self Noise, Input Referred');
+    legend(summary_types,'location','northwest');
+    
+    
+    yl=ylim;
+    y = 20*log10(all_rms_fwav_V);
+    I=find(type == 1);
+    for J=1:length(I);
+        text(x(I(J)),y(I(J))+0.05*diff(yl),{['In ' num2str(teensy_line_in(J))]}, ...
+            'verticalalignment','bottom','horizontalalignment','center');
+    end    
+    I=find(type == ntypes);
+    for J=1:length(I);
+        %if J > 2
+        %    text(x(I(J)),y(I(J))+0.05*diff(yl),{'Gain';[num2str(tympan_gain_dB(J)) 'dB']}, ...
+        %        'verticalalignment','bottom','horizontalalignment','center');
+        %else 
+            text(x(I(J)),y(I(J))-0.03*diff(yl),{'Gain';[num2str(tympan_gain_dB(J)) 'dB']}, ...
+                'verticalalignment','top','horizontalalignment','center');
+        %end
+    end
+
+    
+    
+    subplot(2,2,2);
+    dyn_range_dBV = 20*log10((all_V_atFs/sqrt(2))./all_rms_fwav_V);
+    for Itype=1:ntypes
+        I=find(type==Itype);
+        semilogx(x(I),dyn_range_dBV(I),'o-','linewidth',2);
+        hold on;
+    end
+    hold off;
+    box on
+    ylim([70 100]);
+    ylabel({'Dynamic Range, Input';['(dB, ' bp_txt ')']});
+    xlim([0.01 1.5]);
+    xlabel('Maximum Allowed Input (Vrms) for Gain Setting');
+    set(gca,'XTick',[0.01 0.03 0.1 0.3 1.0],'XTIckLabel',{'0.01' '0.03' '0.1' '0.3' '1.0'});
+    title('Instantaneous Dynamic Range of Input');
+    legend(summary_types,'location','southeast');
+    
+
+    yl=ylim;
+    y = dyn_range_dBV;
+    I=find(type == 1);
+    for J=1:length(I);
+        text(x(I(J)),y(I(J))+0.05*diff(yl),[num2str(y(I(J)),3)], ...
+            'verticalalignment','bottom','horizontalalignment','center');
+    end
+    I=find(type == ntypes);
+    for J=1:length(I);
+        text(x(I(J)),y(I(J))+0.05*diff(yl),[num2str(y(I(J)),3)], ...
+            'verticalalignment','bottom','horizontalalignment','center');
+    end
+end
 
 %% more plots
 if summary_mode == 1
